f4ea71f4c9d98fcb420f06a016c3b81fec6a014b
JimGile <jim@jimgile.com>
2024-07-17 01:38:19 -0600

    Code cleanup

--- src/kaggle_olympic_games_medals.py
+++ src/kaggle_olympic_games_medals.py
@@ -13,7 +13,7 @@ class KaggleOlympicGamesMedals:
     results_file_name = 'olympic_results.csv'
     athletes_file_name = 'olympic_athletes.csv'
 
-    # Replaces long country names with short names
+    # Standardized country names
     country_name_map = {
         'German Democratic Republic (Germany)': 'German Democratic Republic',
         'Federal Republic of Germany': 'Federal Republic of Germany',
@@ -102,6 +102,24 @@ class KaggleOlympicGamesMedals:
         print('Data Loaded')
 
     def explore_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Explore the given DataFrame by printing information about it.
+
+        Parameters:
+            df (pd.DataFrame): The DataFrame to explore.
+
+        Returns:
+            pd.DataFrame: The first few rows of the DataFrame.
+
+        This function takes a DataFrame as input and performs the following actions:
+        1. Prints the information about the DataFrame: the number of rows, columns, data types, and memory usage.
+        2. Prints the number of missing values in each column of the DataFrame.
+        3. Returns the first few rows of the DataFrame.
+
+        Note:
+        - The function assumes that the input DataFrame is a pandas DataFrame.
+        - The function does not modify the input DataFrame.
+        """        
         print("-- Info --")
         print(df.info())
         print("\n-- Missing Info --")
@@ -109,14 +127,30 @@ class KaggleOlympicGamesMedals:
         return df.head()
 
     def get_hosts(self) -> pd.DataFrame:
+        """
+        Get the raw hosts DataFrame by returning a copy of the df_hosts attribute.
+
+        Returns:
+            pd.DataFrame: A copy of the df_hosts attribute
+        """
         return self.df_hosts.copy()
 
     def get_hosts_with_country_codes(self) -> pd.DataFrame:
+        """
+        Retrieves hosts with their respective country codes by merging dataframes and updating game locations.
+
+        Returns:
+            pd.DataFrame: a DataFrame with hosts and their corresponding country codes.
+        """
         df = self.get_hosts()
-        df.loc[df['game_slug'] == 'melbourne-1956', 'game_location'] = 'Australia'
-        df.loc[df['game_slug'] == 'pyeongchang-2018', 'game_location'] = 'North Korea'
-        df.loc[df['game_slug'] == 'seoul-1988', 'game_location'] = 'South Korea'
-        df.loc[df['game_slug'] == 'moscow-1980', 'game_location'] = 'Soviet Union'
+        df.loc[df['game_slug'] == 'melbourne-1956',
+               'game_location'] = 'Australia'
+        df.loc[df['game_slug'] == 'pyeongchang-2018',
+               'game_location'] = 'North Korea'
+        df.loc[df['game_slug'] == 'seoul-1988',
+               'game_location'] = 'South Korea'
+        df.loc[df['game_slug'] == 'moscow-1980',
+               'game_location'] = 'Soviet Union'
 
         df_codes = self.get_country_name_codes()
         df = df.merge(df_codes, how='left',
@@ -127,12 +161,33 @@ class KaggleOlympicGamesMedals:
         return df
 
     def get_medals(self) -> pd.DataFrame:
+        """
+        Get the medals DataFrame by returning a copy of the df_medals attribute.
+
+        Returns:
+            pd.DataFrame: A copy of the df_medals attribute
+        """
         return self.df_medals.copy()
 
-    def get_medals_merged(self) -> pd.DataFrame:
-        return self.clean_data(self.merge_hosts(self.get_medals()))
+    def _get_medals_merged(self) -> pd.DataFrame:
+        """
+        Merge the medals DataFrame with the hosts DataFrame based on the 'slug_game' column,
+        and return the cleaned merged DataFrame.
+        Internal use only.
+
+        Returns:
+            pd.DataFrame: A pandas DataFrame containing the merged and cleaned data.
+        """
+        return self._clean_data(self._merge_hosts(self.get_medals()))
 
     def get_medals_by_country(self) -> pd.DataFrame:
+        """
+        Get the medals DataFrame by dropping duplicate rows based on unique columns,
+        and dropping unnecessary columns from the merged DataFrame.
+
+        Returns:
+            pd.DataFrame: A pandas DataFrame with medals data by country.
+        """
         unique_cols: list[str] = [
             'discipline_title',
             'slug_game',
@@ -142,28 +197,59 @@ class KaggleOlympicGamesMedals:
             'participant_type',
             'country_3_letter_code'
         ]
-        return self.get_medals_merged().drop_duplicates(subset=unique_cols)\
+        return self._get_medals_merged()\
+            .drop_duplicates(subset=unique_cols)\
             .drop(columns=['participant_title', 'athlete_url', 'athlete_full_name', 'country_code'])
 
     def get_medals_by_std_country_name(self) -> pd.DataFrame:
+        """
+        Get the medals DataFrame by standardizing country names based on a mapping from country code to standard name.
+
+        Returns:
+            pd.DataFrame: A pandas DataFrame with standardized country names in the medals data.
+        """
         df = self.get_medals_by_country()
         for key, value in self.country_code_to_std_name_map.items():
             df.loc[df['country_3_letter_code'] == key, 'country_name'] = value
         return df
 
     def get_athletes(self) -> pd.DataFrame:
+        """
+        Get the athletes DataFrame.
+
+        Returns:
+            pd.DataFrame: A copy of the athletes DataFrame.
+        """
         return self.df_athletes.copy()
 
     def get_merged_athletes(self) -> pd.DataFrame:
-        return self.clean_data(self.merge_hosts(self.get_athletes()))
+        """
+        Get the merged athletes DataFrame after cleaning and merging with host data.
+
+        Returns:
+            pd.DataFrame: A pandas DataFrame containing the merged and cleaned athletes data.
+        """
+        return self._clean_data(self._merge_hosts(self.get_athletes()))
 
     def get_results(self) -> pd.DataFrame:
+        """
+        Get the results DataFrame.
+
+        Returns:
+            pd.DataFrame: A copy of the results DataFrame.
+        """
         return self.df_results.copy()
 
     def get_merged_results(self) -> pd.DataFrame:
-        return self.clean_data(self.merge_hosts(self.get_results()))
+        """
+        Get the merged results DataFrame after cleaning and merging with host data.
 
-    def merge_hosts(self, df: pd.DataFrame) -> pd.DataFrame:
+        Returns:
+            pd.DataFrame: A pandas DataFrame containing the merged and cleaned results data.
+        """        
+        return self._clean_data(self._merge_hosts(self.get_results()))
+
+    def _merge_hosts(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Merge the given dataframe with hosts dataframes based on the 'slug_game' column
 
@@ -183,7 +269,7 @@ class KaggleOlympicGamesMedals:
 
         return df_merged
 
-    def clean_data(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _clean_data(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Cleans the data in the DataFrame by applying specific transformations based on column values.
 
@@ -204,11 +290,18 @@ class KaggleOlympicGamesMedals:
             df['discipline_title'] = df['discipline_title'].replace(
                 self.discipline_title_map)
             if 'event_title' in df.columns:
-                self.fix_disciplines_events(df)
+                self._fix_discipline_events(df)
 
         return df
 
-    def fix_disciplines_events(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _clean_event_title(self, et: str) -> str:
+        if et.startswith("Women's"):
+            et = et.replace("Women's", '')+' women'
+        elif et.startswith("Men's"):
+            et = et.replace("Men's", '')+' men'
+        return et.lower().strip()
+
+    def _fix_discipline_events(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Fixes the discipline and event titles in the given DataFrame.
 
@@ -231,9 +324,9 @@ class KaggleOlympicGamesMedals:
                'discipline_title'] = 'Rugby Sevens'
         df.loc[df['event_title'] == 'rugby-7 women',
                'discipline_title'] = 'Rugby Sevens'
-        df.loc[df['event_title'] == 'rugby-7 men', 'event_title'] = 'Men'
-        df.loc[df['event_title'] == 'rugby-7 women', 'event_title'] = 'Women'
-
+        df.loc[df['event_title'] == 'rugby-7 men', 'event_title'] = 'men'
+        df.loc[df['event_title'] == 'rugby-7 women', 'event_title'] = 'women'
+        df['event_title'] = df['event_title'].apply(lambda et: self._clean_event_title(et))
         return df
 
     def pre_process_medal_counts(self, df: pd.DataFrame):
@@ -280,34 +373,40 @@ class KaggleOlympicGamesMedals:
         - season (str): The specific season to filter the data by.
 
         Returns:
-        - pd.DataFrame: A heatmap DataFrame representing the count of participants in each discipline
+        - pd.DataFrame: A heatmap DataFrame representing the count of medals in each discipline
         over the game years.
         """
-        # Make a copy of the DataFrame to avoid modifying the original, filtering by season, and reset the index
-        df = df[df['game_season'] == season].reset_index(drop=True).copy()
-        df_disciplines_year = df.groupby(['discipline_title', 'game_year'])['participant_type']\
-                                .count().reset_index()
+        # Filter by season and reset the index
+        # Group by discipline and game year and count the number of medals
+        df_heatmap = df[df['game_season'] == season].reset_index(drop=True)
+        df_heatmap = df_heatmap.groupby(
+            ['discipline_title', 'game_year']
+        )['participant_type'].count().reset_index()
 
-        df_heatmap = df_disciplines_year.pivot(
+        # Pivot the dataframe with discipline_title values as rows (index) and game_year values as columns
+        df_heatmap = df_heatmap.pivot(
             index='discipline_title', columns='game_year', values='participant_type')
         df_heatmap[df_heatmap > 0] = 1
         column_list = list(df_heatmap.columns)
-        column_last = column_list[-1]
+        most_current_game_year = column_list[-1]
 
-        disciplines_current = df_heatmap[df_heatmap[column_last] == 1].sort_values(
+        # Sort the heatmap so that disciplines with the most recent game year are at the top
+        disciplines_current = df_heatmap[df_heatmap[most_current_game_year] == 1].sort_values(
             column_list)
-        disciplines_current_not = df_heatmap[df_heatmap[column_last] != 1].sort_values(
+        disciplines_older = df_heatmap[df_heatmap[most_current_game_year] != 1].sort_values(
             column_list)
-        df_heatmap = pd.concat([disciplines_current, disciplines_current_not])
+        df_heatmap = pd.concat([disciplines_current, disciplines_older])
+        
+        # Format the index and columns of the heatmap
         df_heatmap.columns = [str(col)[:-2]+'\n'+str(col)[-2:]
                               for col in column_list]
         df_heatmap.index = [idx.replace(' ', '\n', 1)
                             for idx in df_heatmap.index]
         return df_heatmap
 
-    def sort_game_names(self, game_name_list: list[str]) -> list[str]:
+    def _sort_game_names(self, game_name_list: list[str]) -> list[str]:
         """
-        Sorts the list of game names in ascending order based on the last word in each name.
+        Sorts the list of game names in ascending order based on the last word (year) in each name.
 
         Example:
         Input: ['Sydney 2000', 'Atlanta 1996', 'Beijing 2008', 'Athens 2004']
@@ -335,22 +434,23 @@ class KaggleOlympicGamesMedals:
             - country (str): The specific country to filter the data by.
 
         Returns:
-            - pd.DataFrame: A heatmap DataFrame representing the count of participants in each discipline
+            - pd.DataFrame: A heatmap DataFrame representing the count of medals in each discipline
             over the game names.
         """
         # Get data for given country
         df_medal = df[df['country_name'] == country]
 
-        # Group by game_name and discipline_title and count number of participants
-        df_medal = df_medal.groupby(['game_name', 'discipline_title'])['participant_type'].count()\
-            .reset_index()
+        # Group by game_name and discipline_title and count number of medals
+        df_medal = df_medal.groupby(
+            ['game_name', 'discipline_title']
+        )['participant_type'].count().reset_index()
 
         # Reorient dataframe with discipline_title values as rows (index) and game_name values as columns via pivot
         df_medal = df_medal.pivot(
             index='discipline_title', columns='game_name', values='participant_type')
 
         # Reorder columns by sorted game_name column values by year ascending
-        df_medal = df_medal[self.sort_game_names(list(df_medal.columns))]
+        df_medal = df_medal[self._sort_game_names(list(df_medal.columns))]
 
         # Adjust column names depending on number of columns
         if len(list(df_medal.columns)) < 10:
@@ -365,21 +465,75 @@ class KaggleOlympicGamesMedals:
 
         return df_medal
 
+    def get_country_discipline_gender_medal_heatmap(
+            self,
+            df: pd.DataFrame,
+            season: str,
+            country: str,
+            discipline: str,
+            gender: str) -> pd.DataFrame:
+        """
+        This function generates a heatmap DataFrame of medals for a specific country, discipline,
+        and gender in a given season.
+
+        Parameters:
+            df: pd.DataFrame - the dataframe containing the data
+            season: str - the season for which the heatmap is generated
+            country: str - the country for which the heatmap is generated
+            discipline: str - the discipline for which the heatmap is generated
+            gender: str - the gender for which the heatmap is generated
+
+        Returns:
+            pd.DataFrame - the heatmap of medals for the specified country, discipline, and gender
+        """
+        df_discipline = df[
+            (df['game_season'] == season) &
+            (df['country_name'] == country) &
+            (df['discipline_title'] == discipline) &
+            (df['event_gender'] == gender)
+        ].reset_index(drop=True)
+
+        # Group by game_name and event_title and count number of medals
+        df_discipline = df_discipline.groupby(
+            ['game_name', 'event_title']
+        )['participant_type'].count().reset_index()
+
+        # Reorient dataframe with event_title values as rows (index) and game_name values as columns via pivot
+        df_discipline = df_discipline.pivot(
+            index='event_title', columns='game_name', values='participant_type')
+
+        # Reorder columns by sorted game_name column values by year ascending
+        df_discipline = df_discipline[self._sort_game_names(
+            list(df_discipline.columns))]
+        df_discipline.sort_index(ascending=True, inplace=True)
+
+        # Adjust column names depending on number of columns
+        if len(list(df_discipline.columns)) < 10:
+            df_discipline.columns = [col.replace(
+                ' ', '\n') for col in df_discipline.columns]
+        else:
+            df_discipline.columns = [
+                col.split(' ')[-1] for col in df_discipline.columns]
+
+        # Calculate totals by row and overall total
+        df_discipline['Total'] = df_discipline.sum(axis=1)
+        df_discipline.loc['Total'] = df_discipline.sum()
+
+        return df_discipline
+
     def plot_discipline_games_heatmap(self, df: pd.DataFrame, title: str, size=(16, 16), save=False):
         """
-        Plots a heatmap of the given DataFrame, which represents the number of participants in different
-        games and disciplines for a given title.
-
-        :param df: A pandas DataFrame containing the data to be plotted.
+        Plots a heatmap of the disciplines contested at a specific Olympic Games.
         The DataFrame should have columns 'game_name' and 'discipline_title' and rows representing participants.
-        :type df: pd.DataFrame
-        :param title: The title of the heatmap.
-        :type title: str
-        :param size: The size of the plot in inches. Defaults to (16, 16).
-        :type size: tuple, optional
-        :param save: If True, saves the plot as an image file. Defaults to False.
-        :type save: bool, optional
-        :return: None
+
+        Parameters:
+            df (pd.DataFrame): The DataFrame containing the data to be plotted.
+            title (str): The title of the Olympic Games.
+            size (tuple, optional): The size of the figure. Defaults to (16, 16).
+            save (bool, optional): Whether to save the plot as an image. Defaults to False.
+
+        Returns:
+            None
         """
         plt.figure(figsize=size)
         ax = sns.heatmap(df, annot=False, cbar=False,
@@ -395,31 +549,43 @@ class KaggleOlympicGamesMedals:
 
     def plot_country_medal_heatmap(self, df: pd.DataFrame, country: str, season: str, figsize=(16, 16), save=False):
         """
-        Plots a heatmap of the medal count data for a specific country and season.
+        Create a heatmap with annotations representing the medals won by a specific country in a given season.
 
-        :param df: A pandas DataFrame containing the medal count data.
-        :type df: pd.DataFrame
-        :param country: The name of the country for which the medals are plotted.
-        :type country: str
-        :param season: The season for which the medals are being considered.
-        :type season: str
-        :param figsize: The size of the plot. Defaults to (16, 16).
-        :type figsize: tuple, optional
-        :param save: If True, saves the plot as an image file. Defaults to False.
-        :type save: bool, optional
-        :return: None
+        Parameters:
+            df (pd.DataFrame): The DataFrame containing the medal data.
+            country (str): The name of the country for which the medals heatmap is generated.
+            season (str): The season for which the medals heatmap is generated.
+            figsize (tuple, optional): The size of the figure for the heatmap. Defaults to (16, 16).
+            save (bool, optional): A flag indicating whether to save the generated heatmap as an image.
+            Defaults to False.
         """
-        plt.figure(figsize=figsize)
-
         # Create the heatmap with annotations
-        labels = df.map(lambda v: int(v) if not pd.isna(v) else 0)
-        ax = sns.heatmap(df, annot=labels, fmt='g', linewidths=0.8, cmap='coolwarm')
+        plt.figure(figsize=figsize)
+        ax = sns.heatmap(df, annot=True, fmt='g', linewidths=0.8, cmap='coolwarm')
         ax.set_title(f'{country} {season} Medals', size=18)
         ax.xaxis.tick_top()
         plt.tight_layout()
         if save:
-            plt.savefig(
-                f'{country.lower()}_{season.lower()}_medals.png', dpi=300)
+            plt.savefig(f'{country.lower()}_{season.lower()}_medals.png', dpi=200)
+        plt.show()
+
+    def plot_country_discipline_gender_medal_heatmap(
+            self,
+            df: pd.DataFrame,
+            season: str,
+            country: str,
+            discipline: str,
+            gender: str,
+            figsize=(16, 16),
+            save=False):
+
+        plt.figure(figsize=figsize)
+        ax = sns.heatmap(df, annot=True, fmt='g', linewidths=0.8, cmap='coolwarm')
+        ax.set_title(f'{country} {season} {discipline} Medals - {gender}', size=22)
+        ax.xaxis.tick_top()
+        plt.tight_layout()
+        if save:
+            plt.savefig(f'{country.lower()}_{season.lower()}_{discipline.lower()}_{gender.lower()}_medals.png', dpi=200)
         plt.show()
 
     def get_country_name_codes(self) -> pd.DataFrame:
